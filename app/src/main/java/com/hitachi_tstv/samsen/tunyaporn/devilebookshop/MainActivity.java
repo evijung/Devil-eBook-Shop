e0ddc630e82bbfbd6dcd7fa5f72079c6aa4f25e2
evijung <evides01@gmail.com>
2016-07-09 10:15:27 +0700

    Create Sign Up

--- app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/devilebookshop/MainActivity.java
+++ app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/devilebookshop/MainActivity.java
@@ -1,13 +1,22 @@
 package com.hitachi_tstv.samsen.tunyaporn.devilebookshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+//Main Class
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //Main Method
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+    }
+
 }
