acf64dacc1cf1338b1e0452fdc2255c533cbde0f
evijung <evides01@gmail.com>
2016-07-09 14:02:32 +0700

    Post data to server

--- app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/devilebookshop/SignUpActivity.java
+++ app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/devilebookshop/SignUpActivity.java
@@ -5,12 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 //Main Class
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit
     private EditText nameEditText, surnameEditText, userEditText, passwordEditText;
     private String nameString, surnameString, userString, passwordString;
+    private static final String urlPHP = "http://swiftcodingthai.com/9july/add_user_bell.php";
 
 
     @Override
@@ -43,9 +54,38 @@ public class SignUpActivity extends AppCompatActivity {
             myAlert.myDialog(this,"มีช่องว่าง","กรุณากรอกข้อมูลให้ครบด้วยครับ");
         } else {
             //False No Space
-
+            updateNewUserToServer();
 
         }
+    }
+
+    //Update
+    private void updateNewUserToServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("Surname", surnameString)
+                .add("User", userString)
+                .add("Password", passwordString).build();
+
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
     }
 
     private boolean checkSpace() {
