90e88284206ee492fa6f125f30dfde434ff54b74
evijung <evides01@gmail.com>
2016-07-09 10:50:20 +0700

    Design UI Sign Up Activity

--- app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/devilebookshop/SignUpActivity.java
+++ app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/devilebookshop/SignUpActivity.java
@@ -3,11 +3,14 @@ package com.hitachi_tstv.samsen.tunyaporn.devilebookshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Main Class
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
+    //Main Method
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
     }
+
 }
